0ca12f0cd12cb5787b66eaa9da18c97eedd38c68
Endrigo Antonini <antonini@endrigo.com.br>
2013-06-10 13:42:33 -0300

    Partial response of problem 3 and fix the problem 2

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -2,18 +2,19 @@ import sys
 import json
 
 def prepareSentiments(sent_file):
-	sent_file = open(sent_file)
-	sent_scores = []
-	for i, sentLine in enumerate(sent_file):
-		term, score = sentLine.split("\t")
-		sent_scores.append((term, int(score)))
-	return sent_scores
 
+    afinnfile = open(sent_file)
+    sent_scores = {}
+    for i, sentLine in enumerate(afinnfile):
+		word, score = sentLine.split("\t") 
+		sent_scores[word.decode('utf-8')] = float(score)
+    return sent_scores
 
 def readTweets(sent_scores, tweet_file):
 	tweet_file = open(tweet_file)
 	for tweet_line in tweet_file:
 		tweet_sentiment = 0.0
+		unscored_words = set()
 
 		tweet_json = json.loads(tweet_line)
 		if 'text' in tweet_json:
@@ -21,11 +22,16 @@ def readTweets(sent_scores, tweet_file):
 			
 			tweet_splited = tweet_text.split() 
 			for word in tweet_splited:
+				if word in sent_scores:
+					tweet_sentiment += float(sent_scores[word])
+				elif word not in unscored_words:
+					unscored_words.add(word)
 				print word
-
-			tweet_sentiment = search_sentiments_in_text(sent_scores, tweet_text)
+			print unscored_words
+			print tweet_sentiment
+			# tweet_sentiment = search_sentiments_in_text(sent_scores, tweet_text)
 			# print "tweet_sentiment: %s, tweet_text: %s"%(str(tweet_sentiment), tweet_text)
-
+	# print sent_scores
 
 def hw():
     print 'Hello, world!'
